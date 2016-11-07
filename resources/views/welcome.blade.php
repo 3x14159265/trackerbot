e3bbc636d20af2cd699be59e3196789ad4b5fc8e
david p <david@foo.bar>
2016-11-08 00:30:18 +0100

    eb init

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -30,15 +30,15 @@
                     TrackerBot
                 </div>
                 <div class="title m-b-md">
-                    <a href="https://telegram.me/TrackerTestBot" class="btn btn-info btn-tg network">
+                    <a href="https://telegram.me/Klio_Bot" class="btn btn-info btn-tg network">
                         <i class="fa fa-paper-plane"></i>
                         Add to
                         <strong>Telegram</strong>
                     </a>
 
-                    <a href="https://slack.com/oauth/authorize?scope=incoming-webhook,commands,bot&client_id=24628397702.64258146496&state=state" class="network">
+                    <!-- <a href="https://slack.com/oauth/authorize?scope=incoming-webhook,commands,bot&client_id=24628397702.64258146496&state=state" class="network">
                         <img alt="Add to Slack" height="40" width="139" src="https://platform.slack-edge.com/img/add_to_slack.png" srcset="https://platform.slack-edge.com/img/add_to_slack.png 1x, https://platform.slack-edge.com/img/add_to_slack@2x.png 2x"/>
-                    </a>
+                    </a> -->
 
                 </div>
             </div>
